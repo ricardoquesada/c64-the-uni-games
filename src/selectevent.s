cd21005e7a6a3ef90ee1b64e7d89651ac7cd5ba1
Ricardo Quesada <ricardoquesada@gmail.com>
2016-07-10 15:12:41 -0700

    better menu "select event"

--- src/selectevent.s
+++ src/selectevent.s
@@ -35,14 +35,21 @@
 
         ldx #0
 l0:
-        lda selectevent_map,x                   ; copy 7 * 40 chars in total
-        sta SCREEN0_BASE + 40 * 16,x
+        lda selectevent_map,x                   ; copy 9 * 40 chars in total
+        sta SCREEN0_BASE + 40 * 17,x
 
-        lda selectevent_map + 24,x
-        sta SCREEN0_BASE + 40 * 16 + 24,x
+        lda selectevent_map + 64,x
+        sta SCREEN0_BASE + 40 * 17 + 64,x
 
         inx
         bne l0
+
+        lda #1                                  ; white color last $d800 row
+        ldx #39
+l1:     sta $d800 + 24 * 40,x
+        dex
+        bpl l1
+
 
         lda #3                                  ; setup the global variables
         sta MENU_MAX_ITEMS                      ; needed for the menu code
@@ -52,8 +59,8 @@ l0:
         sta MENU_ITEM_LEN
         lda #(40*2)
         sta MENU_BYTES_BETWEEN_ITEMS
-        ldx #<(SCREEN0_BASE + 40 * 18 + 5)
-        ldy #>(SCREEN0_BASE + 40 * 18 + 5)
+        ldx #<(SCREEN0_BASE + 40 * 20 + 5)
+        ldy #>(SCREEN0_BASE + 40 * 20 + 5)
         stx MENU_CURRENT_ROW_ADDR
         sty MENU_CURRENT_ROW_ADDR+1
         ldx #<selectevent_exec
